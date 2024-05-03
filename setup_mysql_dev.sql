ac2a1ec450499ad092bd019760db81d72d0ff874
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-05-02 23:06:48 -0700

    MySQL Setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,13 @@
---   Creates a MySQL server with
---   Database hbnb_dev_db
---   User hbnb_dev with password hbnb_dev_pwd in localhost
---   Grants all privileges for hbnb_dev on hbnb_dev_db
+--   Creates a MySQL server,
+--   Database => hbnb_dev_db,
+--   User hbnb_dev with password hbnb_dev_pwd in localhost,
+--   Grants all privileges for hbnb_dev on hbnb_dev_db,
 --   Grants SELECT privilege for hbnb_dev
 
--- Create the database if it doesn't exist
+-- Create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Create the user if it doesn't exist
+-- Create user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privileges on hbnb_dev_db to hbnb_dev
